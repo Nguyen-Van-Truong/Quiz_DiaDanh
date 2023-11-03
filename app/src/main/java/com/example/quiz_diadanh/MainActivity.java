f3098b7f94a3c9c075e9bea38071d07357f32806
Nguyen-Van-Truong <20130187@st.hcmuaf.edu.vn>
2023-11-03 20:52:18 +0700

    giao dien trang chu 2

--- app/src/main/java/com/example/quiz_diadanh/MainActivity.java
+++ app/src/main/java/com/example/quiz_diadanh/MainActivity.java
@@ -1,36 +1,26 @@
 package com.example.quiz_diadanh;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.drawerlayout.widget.DrawerLayout;
-
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnCreateRoom, btnJoin;
-    DrawerLayout drawerLayout;
-    ActionBarDrawerToggle actionBarDrawerToggle;
-    NavigationView navigationView;
+    NavigationDrawerController drawerController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        drawerLayout = findViewById(R.id.drawer_layout);
-        navigationView = findViewById(R.id.nav_view);
-
-        setupToolbar();
+        setContentView(R.layout.activity_main); // Đảm bảo đây là layout chứa các TextView bạn muốn truy cập
 
         btnCreateRoom = findViewById(R.id.btnCreateRoom);
         btnJoin = findViewById(R.id.btnJoin);
+
+        drawerController = new NavigationDrawerController(this);
+        drawerController.setupDrawer();
 
         btnCreateRoom.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,30 +35,5 @@ public class MainActivity extends AppCompatActivity {
                 // Code cho btnJoin
             }
         });
-
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(MenuItem item) {
-                // Handle navigation item clicks here
-                int id = item.getItemId();
-                if (id == R.id.nav_home) {
-                    // Handle the home action
-                } else if (id == R.id.nav_create_room) {
-                    // Handle the create room action
-                } else if (id == R.id.nav_join) {
-                    // Handle the join action
-                }
-                drawerLayout.closeDrawer(navigationView);
-                return true;
-            }
-        });
-    }
-
-    private void setupToolbar() {
-        androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close);
-        drawerLayout.addDrawerListener(actionBarDrawerToggle);
-        actionBarDrawerToggle.syncState();
     }
 }
