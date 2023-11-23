aacf2fcbc9aa01feb21c05ac492ed31795920e95
Nguyen-Van-Truong <20130187@st.hcmuaf.edu.vn>
2023-11-23 17:54:22 +0700

    Demo load anh len storage trong firebase

--- app/src/main/java/com/example/quiz_diadanh/MainActivity.java
+++ app/src/main/java/com/example/quiz_diadanh/MainActivity.java
@@ -2,36 +2,42 @@ package com.example.quiz_diadanh;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.Firebase;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView txtCreateRoom, txtJoin, test, test2;
     NavigationDrawerController drawerController;
 
-    @SuppressLint("MissingInflatedId")
+    @SuppressLint({"MissingInflatedId", "WrongThread"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
-
-        myRef.setValue("hahaahahaha");
-
+//        demoLoadToRealTimeDB();
+//        demoLoadImgToStorage();
 
         txtCreateRoom = findViewById(R.id.txtCreateRoom);
         txtJoin = findViewById(R.id.txtJoin);
@@ -72,6 +78,38 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    private void demoLoadImgToStorage() {
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReference();
+        StorageReference cau1Ref = storageRef.child("images/cau1.jpg");
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.cau1);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] data = baos.toByteArray();
+
+        UploadTask uploadTask = cau1Ref.putBytes(data);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                System.out.println("that bai");
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                System.out.println("thanh cong");
+            }
+        });
+    }
+
+    private void demoLoadToRealTimeDB() {
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("haha");
     }
 
     public static void setTextViewDrawableSize(TextView textView, int drawableResId, int width, int height) {
